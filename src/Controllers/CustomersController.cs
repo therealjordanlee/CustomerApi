f5227dc37f5bc579d587ab8bb8f1bedc2a85e00d
Jordan Lee <jordan@jordanlee.net>
2020-06-13 09:24:05 +1000

    Added basic CRUD functionality for customers (#1)
    * Added basic CRUD functionality for customers
    * Added repository class for handling db access

--- src/Controllers/CustomersController.cs
+++ src/Controllers/CustomersController.cs
@@ -1,4 +1,6 @@
 ﻿using CustomerApi.Entities;
+using CustomerApi.Models;
+using CustomerApi.Repositories;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -11,17 +13,98 @@ namespace CustomerApi.Controllers
     [Route("customers")]
     public class CustomersController : ControllerBase
     {
-        private readonly CustomerContext _customerContext;
-        public CustomersController(CustomerContext customerContext)
+        // TODO: move customercontext to repository class
+        //private readonly CustomerContext _customerContext;
+        private readonly ICustomerRepository _customerRepository;
+        public CustomersController(ICustomerRepository customerRepository)
         {
-            _customerContext = customerContext;
+            _customerRepository = customerRepository;
         }
 
         [HttpGet("")]
-        public async Task<IActionResult> Get()
+        public async Task<IActionResult> Get([FromQuery]string? firstNameIncludes, [FromQuery]string? lastNameIncludes)
         {
-            var customers = await _customerContext.Customers.ToListAsync();
-            return Ok(customers);
+            if (string.IsNullOrEmpty(firstNameIncludes) && string.IsNullOrEmpty(lastNameIncludes)) // return all customers if query parameters are not specified
+            {
+                //var customers = await _customerContext.Customers.ToListAsync();
+                var customers = await _customerRepository.GetAllCustomersAsync();
+                return Ok(customers);
+            }
+            else
+            {
+                var customers = await _customerRepository.GetCustomersByNameAsync(firstNameIncludes, lastNameIncludes);
+                return Ok(customers);
+            }
+            //var entities = await _customerContext.Customers
+            //    .Where(x => (!string.IsNullOrEmpty(firstNameIncludes) ? x.FirstName.Contains(firstNameIncludes,StringComparison.OrdinalIgnoreCase) : true))
+            //    .Where(x => (!string.IsNullOrEmpty(lastNameIncludes) ? x.LastName.Contains(lastNameIncludes, StringComparison.OrdinalIgnoreCase) : true))
+            //    .ToListAsync();
+        }
+
+        [HttpPost("")]
+        public async Task<IActionResult> CreateCustomer([FromBody]CustomerModel customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _customerRepository.AddCustomerAsync(customer);
+
+            //try
+            //{
+            //    _customerContext.Add(new CustomerEntity
+            //    {
+            //        FirstName = customer.FirstName,
+            //        LastName = customer.LastName,
+            //        DateOfBirth = customer.DateOfBirth
+            //    });
+            //    await _customerContext.SaveChangesAsync();
+            //}
+            //catch(Exception ex)
+            //{
+            //    // todo: error handling
+            //    Console.WriteLine(ex.Message);
+            //}
+            return Ok();
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCustomer([FromRoute] int id,
+            [FromBody]CustomerModel customer)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            await _customerRepository.UpdateCustomerAsync(id, customer);
+
+            //var entity = await _customerContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+            //if(entity != null)
+            //{
+            //    entity.FirstName = customer.FirstName;
+            //    entity.LastName = customer.LastName;
+            //    entity.DateOfBirth = customer.DateOfBirth;
+            //    await _customerContext.SaveChangesAsync();
+            //}
+            //TODO - handle customer not found
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCustomer([FromRoute]int id)
+        {
+            await _customerRepository.DeleteCustomerAsync(id);
+            //var entity = await _customerContext.Customers.FirstOrDefaultAsync(c => c.Id == id);
+            //if(entity != null)
+            //{
+            //    _customerContext.Customers.Remove(entity);
+            //    await _customerContext.SaveChangesAsync();
+            //}
+            //TODO - handle customer not found
+            return Ok();
         }
     }
 }
--- src/Startup.cs
+++ src/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using CustomerApi.Entities;
+using CustomerApi.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -14,15 +15,18 @@ namespace CustomerApi
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //TODO: Add Swagger
+            //TODO: Add logging
+            //TODO: Add logging middleware - https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/write?view=aspnetcore-3.1
+            //TODO: Add correlation id for tracking requests https://github.com/ekmsystems/serilog-enrichers-correlation-id/blob/master/README.md
+            //TODO: Add exception handling middleware? Probably overkill for such a simple API
             services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerDb"));
+            services.AddScoped<ICustomerRepository, CustomerRepository>();
             services.AddControllers();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
