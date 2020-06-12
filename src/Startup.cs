10104c5f3e5420bcbef89bc93fafc9a574ecaf78
Jordan Lee <jordan@jordanlee.net>
2020-06-13 09:48:07 +1000

    Feature/add swagger (#2)
    * added swashbuckle for swagger endpoints
    * cleanup todo notes

--- src/Startup.cs
+++ src/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using CustomerApi.Entities;
 using CustomerApi.Repositories;
@@ -10,6 +12,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CustomerApi
 {
@@ -17,13 +20,19 @@ namespace CustomerApi
     {
         public void ConfigureServices(IServiceCollection services)
         {
-            //TODO: Add Swagger
             //TODO: Add logging
             //TODO: Add logging middleware - https://docs.microsoft.com/en-us/aspnet/core/fundamentals/middleware/write?view=aspnetcore-3.1
             //TODO: Add correlation id for tracking requests https://github.com/ekmsystems/serilog-enrichers-correlation-id/blob/master/README.md
             //TODO: Add exception handling middleware? Probably overkill for such a simple API
             services.AddDbContext<CustomerContext>(opt => opt.UseInMemoryDatabase("CustomerDb"));
             services.AddScoped<ICustomerRepository, CustomerRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Customer API", Version = "v1" });
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
+            });
             services.AddControllers();
         }
 
@@ -35,6 +44,11 @@ namespace CustomerApi
             }
 
             app.UseRouting();
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Customer API V1");
+            });
 
             app.UseEndpoints(endpoints =>
             {
