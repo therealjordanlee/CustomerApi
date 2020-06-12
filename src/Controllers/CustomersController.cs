ec65185aa5001d30b2062042fc0db148ccaaa43a
Jordan Lee <jordan@jordanlee.net>
2020-06-12 19:46:32 +1000

    Initial Commit

---
+++ src/Controllers/CustomersController.cs
@@ -0,0 +1,27 @@
+﻿using CustomerApi.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Controllers
+{
+    [Route("customers")]
+    public class CustomersController : ControllerBase
+    {
+        private readonly CustomerContext _customerContext;
+        public CustomersController(CustomerContext customerContext)
+        {
+            _customerContext = customerContext;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Get()
+        {
+            var customers = await _customerContext.Customers.ToListAsync();
+            return Ok(customers);
+        }
+    }
+}
---
+++ src/Entities/CustomerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Entities
+{
+    public class CustomerContext : DbContext
+    {
+        public DbSet<CustomerEntity> Customers { get; set; }
+        public CustomerContext(DbContextOptions options) : base(options)
+        {
+        }
+    }
+}
