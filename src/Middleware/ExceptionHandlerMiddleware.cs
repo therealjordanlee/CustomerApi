94d3ff306340648e7dc5e7123ebc3a4624d5b645
Jordan Lee <jordan@jordanlee.net>
2020-06-13 10:03:35 +1000

    added exception handling middleware

---
+++ src/Middleware/ExceptionHandlerMiddleware.cs
@@ -0,0 +1,43 @@
+﻿using CustomerApi.Exceptions;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CustomerApi.Middleware
+{
+    public class ExceptionHandlerMiddleware
+    {
+        private readonly RequestDelegate next;
+        public ExceptionHandlerMiddleware(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception ex)
+            {
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception ex)
+        {
+            var code = HttpStatusCode.InternalServerError; // 500 if unexpected
+            if (ex is CustomerNotFoundException) code = HttpStatusCode.NotFound;
+
+            var result = JsonSerializer.Serialize(new { error = ex.Message });
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)code;
+            return context.Response.WriteAsync(result);
+        }
+    }
+}
