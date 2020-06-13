07d28f65fb89b3b0ab644442a9ae573c0cfc7729
Jordan Lee <jordan@jordanlee.net>
2020-06-13 10:06:23 +1000

    Feature/exception handling middleware (#3)
    * added exception handling middleware
    * refactor repository class to remove duplicate error handling code

--- src/Startup.cs
+++ src/Startup.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Reflection;
 using System.Threading.Tasks;
 using CustomerApi.Entities;
+using CustomerApi.Middleware;
 using CustomerApi.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -43,6 +44,7 @@ namespace CustomerApi
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseMiddleware(typeof(ExceptionHandlerMiddleware));
             app.UseRouting();
             app.UseSwagger();
             app.UseSwaggerUI(c =>
